5924a5c1ac692236412a50031e02001ee6606911
Gonzalo Barrera <gonzalofotoguitarra@gmail.com>
2023-11-22 13:25:03 -0300

    added flex layout

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
 
-export default function App() {
+const Card = ({ item }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Text style={styles.square}>{item}</Text>
+  )
+
+}
+export default function App() {
+  const totalItemsSet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const totalItems = Array.from(totalItemsSet); // Evitamos duplicados
+  let duplicated = totalItems.concat(totalItems); // Duplicamos
+  // TODO, crear la estructura para que cada item
+  // tenga el estado "oculto,visible y emparejado"  
+  duplicated.sort(() => Math.random() - 0.5); // randomizamos
+
+  return (
+    <SafeAreaView style={styles.safe} >
+      <View style={styles.container}>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          {(duplicated).map((item, index) => {
+            return <Card item={item} key={index} />
+          })}
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.exit}>SALIR</Text>
+        </View>
+      </View>
+
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexBasis: '100%',
+    backgroundColor: '#2C3E50',
+    marginTop: 30
   },
+  square: {
+    backgroundColor: '#3498DB',
+    marginTop: 40,
+    margin: 15,
+    padding: 28,
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#ECF0F1'
+  },
+  safe: {
+    flex: 1
+  },
+  exit: {
+    color: '#ECF0F1',
+    textAlign: 'center',
+    fontSize: 22
+  }
 });
